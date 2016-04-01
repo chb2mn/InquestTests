114714f84fb1c81a6fd1a2e0f24ec69567bf5213
cburkhalter <cburkhalter@novetta.com>
2016-04-01 17:13:06 -0400

    finished parsing xml

--- parse/parse_xml.py
+++ parse/parse_xml.py
@@ -1,5 +1,36 @@
 import urllib2
+import json
+import re
+import xml.etree.ElementTree as ET
+import types
+from pprint import pprint
 
-response = urllib2.urlopen('http://www.malwaredomainlist.com/hostslist/mdl.xml')
-data = response.read()
-print data
+def get_data(url):
+    response = urllib2.urlopen(url)
+    data = response.read()
+    return data
+
+def form_dict(root):
+    json_dict = {}
+    if root.text is not None and len(root.text.strip()) > 0:
+        json_dict["text"] = root.text
+    if len(root.attrib) > 0:
+        json_dict["attributes"] = root.attrib
+    for child in list(root):
+        if child.tag in json_dict:
+            try:
+                json_dict[child.tag].append(form_dict(child))
+            except AttributeError:
+                json_dict[child.tag] = [json_dict[child.tag], form_dict(child)]
+        else:            
+            json_dict[child.tag] = form_dict(child)
+    return json_dict
+
+if __name__ == '__main__':
+    #xml = get_data('http://www.malwaredomainlist.com/hostslist/mdl.xml')
+    xml = open("mdl.xml", 'rb').read()
+    root = ET.fromstring(xml)
+    my_dict = form_dict(root)
+    fout = open("mdl.json", 'wb')
+    json.dump(my_dict, fout)
+    pprint(my_dict)
