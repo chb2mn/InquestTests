2067e7e8c2766ee7265d604fa4caea467f627054
cburkhalter <cburkhalter@novetta.com>
2016-04-04 11:15:11 -0400

    abstracting the aws credentials into .cfg

--- explore/aws.py
+++ explore/aws.py
@@ -1,12 +1,15 @@
 import boto.sdb
 from pprint import pprint
+import ConfigParser
 
 if __name__ == '__main__':
+    config = ConfigParser.RawConfigParser()
+    config.read('aws.cfg')
     print "connecting, please wait"
     conn = boto.sdb.connect_to_region(
-            'us-east-1',
-            aws_access_key_id = 'AKIAJDFJ22X4S2UZK52A',
-            aws_secret_access_key = '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',
+            config.get('aws_configs', 'region'),
+            aws_access_key_id = config.get('aws_configs', 'aws_access_key_id'),
+            aws_secret_access_key = config.get('aws_configs', 'aws_secret_access_key'),
         )
     domain = conn.get_domain('sandbox')
     #NOTE: For big data running select * is stupid, but we don't have a lot so this is ok
